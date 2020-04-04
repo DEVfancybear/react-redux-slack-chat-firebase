99a4a91433327ac42bdf825458fc1d7c26c38795
DEVfancybear <duonghacker2345@gmail.com>
2020-04-04 19:07:58 +0700

    add channel title users count to component MessageHeader

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -9,7 +9,8 @@ const Messages = ({currentChannel, currentUser}) => {
     const [state, setState] = useState({
         messagesRef: firebase.database().ref("messages"),
         messages: [],
-        messagesLoading: true
+        messagesLoading: true,
+        numUniqueUsers: ""
     });
     useEffect(() => {
         if (currentChannel && currentUser) {
@@ -29,7 +30,23 @@ const Messages = ({currentChannel, currentUser}) => {
                 messages: loadedMessages,
                 messagesLoading: false
             });
+            countUniqueUsers(loadedMessages);
         });
+    };
+
+    const countUniqueUsers = messages => {
+        const uniqueUsers = messages.reduce((acc, message) => {
+            if (!acc.includes(message.user.name)) {
+                acc.push(message.user.name);
+            }
+            return acc;
+        }, []);
+        const plural = uniqueUsers.length > 1 || uniqueUsers.length === 0;
+        const numUniqueUsers = `${uniqueUsers.length} user${plural ? "s" : ""}`;
+        setState({
+            ...state,
+            numUniqueUsers
+        })
     };
 
     const displayMessages = messages =>
@@ -41,9 +58,10 @@ const Messages = ({currentChannel, currentUser}) => {
                 user={currentUser}
             />
         ));
+    const displayNameChannel = channel => channel ? `#${channel.name}` : "";
     return (
         <Fragment>
-            <MessagesHeader/>
+            <MessagesHeader numUniqueUsers={state.numUniqueUsers} displayNameChannel={displayNameChannel(currentChannel)}/>
             <Segment>
                 <CommentGroup className="messages">{displayMessages(state.messages)}</CommentGroup>
             </Segment>
--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Header, Segment, Input, Icon } from "semantic-ui-react";
+import {Header, Segment, Input, Icon} from "semantic-ui-react";
 
-const MessagesHeader = () => {
+const MessagesHeader = ({displayNameChannel, numUniqueUsers}) => {
     return (
         <Segment clearing>
             {/* Channel Title */}
-            <Header fluid="true" as="h2" floated="left" style={{ marginBottom: 0 }}>
+            <Header fluid="true" as="h2" floated="left" style={{marginBottom: 0}}>
           <span>
-            Channel
-            <Icon name={"star outline"} color="black" />
+              {displayNameChannel}
+              <Icon name={"star outline"} color="black"/>
           </span>
-                <Header.Subheader>2 Users</Header.Subheader>
+                <Header.Subheader>{numUniqueUsers}</Header.Subheader>
             </Header>
 
             {/* Channel Search Input */}
