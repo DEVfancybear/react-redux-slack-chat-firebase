bf838f243594437547997c022334fa4cb9afe726
DEVfancybear <duonghacker2345@gmail.com>
2020-04-04 20:26:19 +0700

    add search messages

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,40 +1,79 @@
-import React, {Fragment, useState, useEffect} from "react";
-import {Segment, CommentGroup} from "semantic-ui-react"
+import React, {Component,Fragment} from "react";
+import {Segment, Comment} from "semantic-ui-react";
+import firebase from "../../firebase";
+
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
-import firebase from "../../firebase";
 import Message from "./Message";
 
-const Messages = ({currentChannel, currentUser}) => {
-    const [state, setState] = useState({
-        messagesRef: firebase.database().ref("messages"),
-        messages: [],
-        messagesLoading: true,
-        numUniqueUsers: ""
-    });
-    useEffect(() => {
-        if (currentChannel && currentUser) {
-            addListeners(currentChannel.id);
+class Messages extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messagesRef: firebase.database().ref("messages"),
+            messages: [],
+            messagesLoading: true,
+            channel: this.props.currentChannel,
+            user: this.props.currentUser,
+            numUniqueUsers: "",
+            searchTerm: "",
+            searchLoading: false,
+            searchResults: []
+        };
+    }
+
+
+    componentDidMount() {
+        const {channel, user} = this.state;
+
+        if (channel && user) {
+            this.addListeners(channel.id);
         }
-    }, [currentUser, currentChannel]);
-    const addListeners = channelId => {
-        addMessageListener(channelId);
+    }
+
+    addListeners = channelId => {
+        this.addMessageListener(channelId);
     };
 
-    const addMessageListener = channelId => {
+    addMessageListener = channelId => {
         let loadedMessages = [];
-        state.messagesRef.child(channelId).on("child_added", snap => {
+        this.state.messagesRef.child(channelId).on("child_added", snap => {
             loadedMessages.push(snap.val());
-            setState({
-                ...state,
+            this.setState({
                 messages: loadedMessages,
                 messagesLoading: false
             });
-            countUniqueUsers(loadedMessages);
+            this.countUniqueUsers(loadedMessages);
         });
     };
 
-    const countUniqueUsers = messages => {
+    handleSearchChange = event => {
+        this.setState(
+            {
+                searchTerm: event.target.value,
+                searchLoading: true
+            },
+            () => this.handleSearchMessages()
+        );
+    };
+
+    handleSearchMessages = () => {
+        const channelMessages = [...this.state.messages];
+        const regex = new RegExp(this.state.searchTerm, "gi");
+        const searchResults = channelMessages.reduce((acc, message) => {
+            if (
+                (message.content && message.content.match(regex)) ||
+                message.user.name.match(regex)
+            ) {
+                acc.push(message);
+            }
+            return acc;
+        }, []);
+        this.setState({searchResults});
+        setTimeout(() => this.setState({searchLoading: false}), 1000);
+    };
+
+    countUniqueUsers = messages => {
         const uniqueUsers = messages.reduce((acc, message) => {
             if (!acc.includes(message.user.name)) {
                 acc.push(message.user.name);
@@ -43,31 +82,50 @@ const Messages = ({currentChannel, currentUser}) => {
         }, []);
         const plural = uniqueUsers.length > 1 || uniqueUsers.length === 0;
         const numUniqueUsers = `${uniqueUsers.length} user${plural ? "s" : ""}`;
-        setState({
-            ...state,
-            numUniqueUsers
-        })
+        this.setState({numUniqueUsers});
     };
 
-    const displayMessages = messages =>
+    displayMessages = messages =>
         messages.length > 0 &&
         messages.map(message => (
             <Message
                 key={message.timestamp}
                 message={message}
-                user={currentUser}
+                user={this.state.user}
             />
         ));
-    const displayNameChannel = channel => channel ? `#${channel.name}` : "";
-    return (
-        <Fragment>
-            <MessagesHeader numUniqueUsers={state.numUniqueUsers} displayNameChannel={displayNameChannel(currentChannel)}/>
-            <Segment>
-                <CommentGroup className="messages">{displayMessages(state.messages)}</CommentGroup>
-            </Segment>
-            <MessageForm currentChannel={currentChannel} currentUser={currentUser} messagesRef={state.messagesRef}/>
-        </Fragment>
-    )
+
+    displayChannelName = channel => (channel ? `#${channel.name}` : "");
+
+    render() {
+        // prettier-ignore
+        const {messagesRef, messages, channel, user, numUniqueUsers, searchTerm, searchResults, searchLoading} = this.state;
+
+        return (
+            <Fragment>
+                <MessagesHeader
+                    channelName={this.displayChannelName(channel)}
+                    numUniqueUsers={numUniqueUsers}
+                    handleSearchChange={this.handleSearchChange}
+                    searchLoading={searchLoading}
+                />
+
+                <Segment>
+                    <Comment.Group className="messages">
+                        {searchTerm
+                            ? this.displayMessages(searchResults)
+                            : this.displayMessages(messages)}
+                    </Comment.Group>
+                </Segment>
+
+                <MessageForm
+                    messagesRef={messagesRef}
+                    currentChannel={channel}
+                    currentUser={user}
+                />
+            </Fragment>
+        );
+    }
 }
 
-export default Messages
+export default Messages;
--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {Header, Segment, Input, Icon} from "semantic-ui-react";
 
-const MessagesHeader = ({displayNameChannel, numUniqueUsers}) => {
+const MessagesHeader = ({channelName, numUniqueUsers, handleSearchChange,searchLoading}) => {
     return (
         <Segment clearing>
             {/* Channel Title */}
             <Header fluid="true" as="h2" floated="left" style={{marginBottom: 0}}>
           <span>
-              {displayNameChannel}
+              {channelName}
               <Icon name={"star outline"} color="black"/>
           </span>
                 <Header.Subheader>{numUniqueUsers}</Header.Subheader>
@@ -16,6 +16,8 @@ const MessagesHeader = ({displayNameChannel, numUniqueUsers}) => {
             {/* Channel Search Input */}
             <Header floated="right">
                 <Input
+                    loading={searchLoading}
+                    onChange={handleSearchChange}
                     size="mini"
                     icon="search"
                     name="searchTerm"
